be0831795b9dcd6f7d61043b192267b801f9fc21
IfyN <ifynwosu0@gmail.com>
2022-11-07 15:11:52 +0100

    import SCSS styles into scoped component

---
+++ chapter1/Exercise1.03/src/components/Exercise1-03.vue
@@ -0,0 +1,32 @@
+<template>
+   <div>
+      <h1> {{ title }}</h1>
+      <h2> {{ subtitle }}</h2>
+      <ul>
+         <li> {{ items[0] }}</li>
+         <li> {{ items[1] }}</li>
+         <li> {{ items[2] }}</li>
+      </ul>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/typography';
+
+h1 {
+   font-size: 50px;
+   color: $color-blue;
+}
+</style>
+
+<script>
+export default {
+   data() {
+      return {
+         title: 'A list component',
+         subtitle: 'A subtitle',
+         items: ["Cap", "Blue", "Ship"]
+      }
+   }
+}
+</script>
